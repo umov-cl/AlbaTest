cc53c3bce2ba86062e4f0a25bfe493abf2363370
dwexman <wexmandenise@gmail.com>
2025-03-26 15:20:55 -0300

    no sounds

--- src/screen/ExerciseFollowThePathSelectScreen.tsx
+++ src/screen/ExerciseFollowThePathSelectScreen.tsx
@@ -1,17 +1,12 @@
 import { useState } from 'react';
 import { Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import useSound from "use-sound"; 
 import PathSelection from '../components/PathSelection';
 import AngleSelection from '../components/AngleSelection';
 import LapSelection from '../components/LapSelection';
 import LimbSelection from '../components/LimbSelection';
 import HoldingTypeSelection from '../components/HoldingTypeSelection';
 
-const sounds: Record<string, { default: string }> = import.meta.glob(
-    "../assets/sounds/**/*.mp3",
-    { eager: true }
-);
 
 export default function ExerciseFollowThePathSelectScreen() {
   const navigate = useNavigate();
@@ -21,18 +16,14 @@ export default function ExerciseFollowThePathSelectScreen() {
   const [limb, setLimb] = useState<string | null>(null);
   const [holding, setHolding] = useState<string | null>(null);
 
-  const [playNext] = useSound(sounds["../assets/sounds/next.mp3"]?.default || "");
-  const [playBack] = useSound(sounds["../assets/sounds/back.mp3"]?.default || "");
 
   const handleNextStep = () => {
-    playNext();
     setTimeout(() => {
       setStep((prev) => prev + 1);
     }, 500); 
   };
 
   const handleStepBack = () => {
-    playBack(); 
     if (step > 1) {
       setStep((prev) => prev - 1);
     } else {
